e5b7a2eef8512481a8e765ce309a15b6f7f67534
Anuark <jaicof@gmail.com>
2022-05-21 15:43:10 -0600

    different links on rescue and success pages

--- frontend/src/views/Header.js
+++ frontend/src/views/Header.js
@@ -3,12 +3,14 @@
 import React from 'react';
 import { ethers } from 'ethers';
 import { Navbar, Nav, Button, Container } from 'react-bootstrap';
-import { useResovedPath } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 // import Logo from '../assets/logo.png';
 import Logo from '../assets/baywatch_logo.png';
 
 const Header = (props) => {
   const { signer, setProvider, setSigner } = props;
+  const location = useLocation();
+
   const connectWallet = async () => {
     await ethereum.request({ method: 'eth_requestAccounts' });
     const provider = new ethers.providers.Web3Provider(ethereum);
@@ -33,18 +35,29 @@ const Header = (props) => {
           />{' '}
         </Navbar.Brand>*/}
 
-        <Nav defaultActiveKey="/home" as="ul" className="me-auto justify-content-center">
-          <Nav.Link className="text-white" href="#what">
-            What we do
-          </Nav.Link>
-          <Nav.Link className="text-white" href="#how">
-            How we do it
-          </Nav.Link>
-          <Nav.Link className="text-white" href="#who">
-            Who is it for
-          </Nav.Link>
-          {/* <Nav.Link href="/">Submit Proposal</Nav.Link> */}
-        </Nav>
+        { ['/', '/home'].includes(location.pathname) ?
+          <Nav defaultActiveKey="/home" as="ul" className="me-auto justify-content-center">
+            <Nav.Link className="text-white" href="#what">
+              What we do
+            </Nav.Link>
+            <Nav.Link className="text-white" href="#how">
+              How we do it
+            </Nav.Link>
+            <Nav.Link className="text-white" href="#who">
+              Who is it for
+            </Nav.Link>
+            <Nav.Link className="text-white" href="/rescue">
+              Rescue assets
+            </Nav.Link>
+            {/* <Nav.Link href="/">Submit Proposal</Nav.Link> */}
+          </Nav>
+          :
+          <Nav>
+            <Nav.Link className="text-white" href="/rescue">
+              Rescue assets
+            </Nav.Link>
+          </Nav>
+        }
         <Nav>
           <Nav.Item className="text-white">{Object.keys(signer).length === 0 ? <Button onClick={connectWallet}>Connect Wallet</Button> : '⚡️ Wallet Connected'}</Nav.Item>
         </Nav>
